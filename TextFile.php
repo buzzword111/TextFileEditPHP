ba0b60a9649c0f2b9d696a044c6055e27630be09
buzzword111 <buzzword111@gmail.com>
2014-02-12 13:20:11 +0900

    画像アップロード機能追加とバグ修正

--- TextFile.php
+++ TextFile.php
@@ -2,34 +2,60 @@
 
 class TextFile{
 	// 入力された日付
-	protected $date;
+	protected $date = '';
 	// 入力されたメッセージ
-	protected $message;
+	protected $message = '';
+    // 画像名
+    protected $imageName = '';
+    // 入力された画像の表示位置
+    protected $imagePos = 0;
 
-	// 入力されたメッセージ
-	protected $result;
+	// 処理結果
+	public $result = '';
 
 	// タグのエスケープ判断フラグ
-	protected $tagEscapeFlg = false;
+	// protected $tagEscapeFlg = false;
+
+
+//-------------
+//    定数
+//-------------
 	// 編集するファイルパス
 	const FILE_PATH = "./sample.txt";
+    // ファイルアップロード時の保存パス
+    const FILE_SAVE_PATH = "/var/www/html/";
+
+    // ファイルオープン関数の　引数一覧
+    const FILE_READ_MODE  = 0;  // 読み取り専用の場合
+    const FILE_WRITE_MODE = 1;  // 書き込み専用の場合
+    const FILE_ADD_MODE   = 2;  // 追加専用の場合
+
+    // 画像表示位置の 値と位置の関係
+    const IMAGE_POS_TOP    = 0;  // 文章の上に表示
+    const IMAGE_POS_BOTTOM = 1;  // 文章の下に表示
+
+    // 書き込みタグ文字列 日付,画像,文章の場合
+    const WRITE_TAG_STR1 = '<span class="date">%s</span><br><img src="%s" width="200px"><div>%s</div>';
+    const WRITE_TAG_STR1_PATTERN = '/<span class="date">.*<\/span><br><img src=".*" width="200px"><div>.*<\/div>/';
+
+    // 書き込みタグ文字列 日付,文章,画像の場合
+    const WRITE_TAG_STR2 = '<span class="date">%s</span><div>%s</div><img src="%s" width="200px">';
+    const WRITE_TAG_STR2_PATTERN = '/<span class="date">.*<\/span><div>.*<\/div><img src=".*" width="200px">/';
+
+    // ファイルアップロード時に、許可する拡張子
+    private $errorChkExtension = array("gif","jpg", "jpeg", "png", "bmp");
+//--------------
 
 
+
+    //---------------
+    //  登録メソッド
+    //---------------
 	public function regist(){
 
-		// ファイルオープン
-        $fp = fopen( TextFile::FILE_PATH, 'ab');
-        if( !$fp ){
-        	$this->result = "ファイルオープンに失敗しました";
-        	return false;
-        }
-
-        // ファイルロック
-        $lockResult = flock($fp, LOCK_SH);
-        if( !$lockResult ){
-            $this->result = "ファイルロックに失敗しました";
-        	return false;
-        }
+        // ファイルオープン と　ロック
+        $fp = $this->fileOpenAndLock(TextFile::FILE_ADD_MODE);
+        if(!$fp) return false;
 
         //--------------
         //  ファイル登録
@@ -38,211 +64,164 @@ class TextFile{
         // メッセージ内の改行を<br>タグに置き換える
         $this->message = str_replace(array("\r\n","\r","\n"), "<br>", $_POST["message"]);
 
-        $data = sprintf('<span class="date">%s</span><br>%s',$this->date, $this->message);
-        $data = $data . "\n";
-        fwrite($fp,  $data);
+        // ファイルアップロード
+        if( !$this->fileUpload() ) return false;
 
-        flock($fp, LOCK_UN);
-        fclose($fp);
+        switch($_POST["image_pos"]){
+            case TextFile::IMAGE_POS_TOP:
+                $data = sprintf(TextFile::WRITE_TAG_STR1,$this->date, $this->imageName, $this->message);
+                break;
+            case TextFile::IMAGE_POS_BOTTOM:
+                $data = sprintf(TextFile::WRITE_TAG_STR2,$this->date, $this->message, $this->imageName);
+                break;
+        }
+
+        $data = "\n" . $data;
+        fwrite($fp,  $data);
         //----------------------------------
+
+        // ファイルクローズ と アンロック
+        if( $this->fileCloseAndUnlock($fp) ) return false;
 
         $this->result = "ファイルの登録に成功しました";
         return true;
 	}
 
 
+    //-------------------------
+    //  ファイル内容表示メソッド
+    //-------------------------
 	public function viewFileValue(){
 
-		// ファイルオープン
-        $fp = fopen( TextFile::FILE_PATH, 'r');
-        if( !$fp ){
-        	$this->result = "ファイルオープンに失敗しました";
-        	return false;
-        }
-
-        // ファイルロック
-        $lockResult = flock($fp, LOCK_SH);
-        if( !$lockResult ){
-            $this->result = "ファイルロックに失敗しました";
-        	return false;
-        }
-
+		// ファイル内容を改行コードごとに、Arrayへ設定する
+        $fileArray = $this->getFileValueArray();
+        if( !$fileArray ) return false;
 
         //------------------
         //  ファイル内容表示
         //------------------
-
-		// ファイル内容を改行コードごとに、Arrayへ設定する
-
-		$fileValue = file_get_contents(TextFile::FILE_PATH);
-		str_replace(array("\r\n","\r"), "\n", $fileValue);
-		$fileArray = split("\n",$fileValue);
-
-        // ファイル内容を後ろから表示
-
         print('<table class="table">');
 		$fileArrayLength = count($fileArray);
         for($i = 0; $i<$fileArrayLength; $i++){
+            // 後ろから取り出す
             $value = array_pop($fileArray);
 
+            // 空文字であればスキップ
             if($value == ""){
             	continue;
             }
             
             // 日付のみ取得
-            $pattern= "/\d{4}\/\d{1,2}\/\d{1,2}/";
+            $pattern = "/\d{4}\/\d{1,2}\/\d{1,2}/";
             preg_match($pattern, $value , $this->date );
-            // $this->date = mb_ereg_replace('[^0-9]', '.', $this->date[0]);
 
             // メッセージのみ取得
             $value = htmlspecialchars_decode($value);
+            $pattern = "/<div>.*<\/div>/";
+            preg_match($pattern, $value , $this->message);
+            $this->message = mb_substr($this->message[0], stripos( $this->message[0], ">")+1, strripos( $this->message[0], "<")-5);
 
-            $this->message = mb_substr($value, stripos( $value, "<br>")+4);
+            // 画像名のみ取得
+            $pattern = '/src=".*?"/';
+            preg_match($pattern, $value , $this->imageName);
+            $this->imageName = mb_substr($this->imageName[0], stripos( $this->imageName[0], '"')+1, strripos( $this->imageName[0], '"') - 5);
 
+            // 画像表示位置取得
+            if( preg_match(TextFile::WRITE_TAG_STR1_PATTERN, $value) ) {
+                $this->imagePos = TextFile::IMAGE_POS_TOP;
+            }
+            if( preg_match(TextFile::WRITE_TAG_STR2_PATTERN, $value) ) {
+                $this->imagePos = TextFile::IMAGE_POS_BOTTOM;
+            }
+            // 表示
             $write  = '<TR><TD data-toggle="modal" data-target="#formModal" ';
-            $write .= sprintf('onclick="doChangeUpdateForm(\'%s\', \'%s\');" >', $this->date[0], $this->message);
+            $write .= sprintf('onclick="doChangeUpdateForm(\'%s\', \'%s\', \'%s\', %s);" >', $this->date[0], $this->message, $this->imageName, $this->imagePos);
             print( $write );
             print($value);
             print('</TD></TR>');
         }
         print('</table>');
-        
-        flock($fp, LOCK_UN); // ファイルロックを解放
-        fclose($fp);         // ファイルをクローズする
         //----------------------------------
 
         return true;
 	}
 
+    //---------------
+    //  更新メソッド
+    //---------------
 	public function update(){
 
-		// ファイルオープン
-		$fp = fopen( TextFile::FILE_PATH, 'r');
-        if( !$fp ){
-        	$this->result = "ファイルオープンに失敗しました";
-        	return false;
-        }
+        // 変更対象データ = 変更前データを取得
+        $preData = $this->getPreData();
 
-        // ファイルロック
-        $lockResult = flock($fp, LOCK_SH);
-        if( !$lockResult ){
-            $this->result = "ファイルロックに失敗しました";
-        	return false;
-        }
+        // 変更データを取得
+        $updateData = $this->getUpdateData();
+        // ファイルアップロード時にエラーが起きた場合
+        if( $updateData === false) return false;
 
+        //----------------------------------------
+        //  ファイルの対象行 更新処理
+        //  仕様: テキストファイルを読み込んで作り直す。 
+        //----------------------------------------
 
-        //------------------
-        //  ファイル内容表示
-        //------------------
+        // ファイル内容を改行コードごとに、Arrayへ設定する
+        $fileArray = $this->getFileValueArray();
+        if( !$fileArray ) return false;
 
-		// ファイル内容を改行コードごとに、Arrayへ設定する
-		$fileArray = array();
-        while( !feof($fp) ){
-            $value = fgets($fp);
-            if($value == ""){
-            	continue;
-            }
-
-            $value = str_replace(array("\r\n","\r","\n"), "", $value);
-	    	// Arrayに追加する
-            array_push($fileArray, $value);
-        }
-
-        flock($fp, LOCK_UN); // ファイルロックを解放
-        fclose($fp);         // ファイルをクローズする
-
-		$fp = fopen( TextFile::FILE_PATH, 'w');
-
-		// $lockResult = flock($fp, LOCK_SH);
-
-        $this->date    = $_POST["date"];
-        // メッセージ内の改行を<br>タグに置き換える
-        $this->message = str_replace(array("\r\n","\r","\n"), "<br>", $_POST["message"]);
-        $data = sprintf('<span class="date">%s</span><br>%s',$this->date, $this->message);
-
-        $preDate    = $_POST["pre_date"];
-        $preMessage = str_replace(array("\r\n","\r","\n"), "<br>", $_POST["pre_message"]);
-        $preData = sprintf('<span class="date">%s</span><br>%s',$preDate, $preMessage);
-
-
-        $updateKey = null;
-        // ファイル内容配列を更新
+        $updateKey = null; // 更新対象行のKey
+        // 更新対象行を検索
         foreach ($fileArray as $key => $value) {
-    		var_dump($value);
         	// 更新する行と一致すれば
         	if($value === $preData){
-        		var_dump("一致しましたよ！");
         		$updateKey = $key;
         	}
         }
-        // 書き込み
+
+        if($updateKey === null){
+            $this->result = "ファイルの更新対象行 が見つかりませんでした";
+            return false;
+        }
+        // 更新対象行を入力したデータに上書きする。
+        $fileArray[$updateKey] = $updateData;
+
+        // ファイルオープン と　ロック
+        $fp = $this->fileOpenAndLock(TextFile::FILE_WRITE_MODE);
+        if(!$fp) return false;
+
+        // ファイル書き込み
         foreach($fileArray as $key => $value){
-        	if($key === $updateKey){
-        		$value = $data;
-        	}
+            if( $value == "") continue;
+
+            $value = $value . "\n";
             fwrite($fp,$value);
         }
+        //----------------------------------------
 
-        flock($fp, LOCK_UN); // ファイルロックを解放
-        fclose($fp);         // ファイルをクローズする
+        // ファイルクローズ と アンロック
+        if( $this->fileCloseAndUnlock($fp) ) return false;
 
-        //----------------------------------
-
+        $this->result = "ファイルの対象行 更新に成功しました";
         return true;
 	}
 
+    //---------------
+    //  削除メソッド
+    //---------------
 	public function delete(){
 
-		// ファイルオープン
-		$fp = fopen( TextFile::FILE_PATH, 'r');
-        if( !$fp ){
-        	$this->result = "ファイルオープンに失敗しました";
-        	return false;
-        }
+        // 変更対象データ = 変更前データを取得
+        $preData = $this->getPreData();
 
-        // ファイルロック
-        $lockResult = flock($fp, LOCK_SH);
-        if( !$lockResult ){
-            $this->result = "ファイルロックに失敗しました";
-        	return false;
-        }
+        //----------------------------------------
+        //  ファイルの対象行 削除処理
+        //  仕様: テキストファイルを読み込んで作り直す。 
+        //----------------------------------------
 
+        // ファイル内容を改行コードごとに、Arrayへ設定する
+        $fileArray = $this->getFileValueArray();
+        if( !$fileArray ) return false;
 
-        //------------------
-        //  ファイル内容表示
-        //------------------
-
-		// ファイル内容を改行コードごとに、Arrayへ設定する
-		$fileArray = array();
-        while( !feof($fp) ){
-            $value = fgets($fp);
-            if($value == ""){
-            	continue;
-            }
-
-            $value = str_replace(array("\r\n","\r","\n"), "", $value);
-	    	// Arrayに追加する
-            array_push($fileArray, $value);
-        }
-
-        flock($fp, LOCK_UN); // ファイルロックを解放
-        fclose($fp);         // ファイルをクローズする
-
-		$fp = fopen( TextFile::FILE_PATH, 'w');
-
-		// $lockResult = flock($fp, LOCK_SH);
-
-        $this->date    = $_POST["date"];
-        // メッセージ内の改行を<br>タグに置き換える
-        $this->message = str_replace(array("\r\n","\r","\n"), "<br>", $_POST["message"]);
-        $data = sprintf('<span class="date">%s</span><br>%s',$this->date, $this->message);
-
-        $preDate    = $_POST["pre_date"];
-        $preMessage = str_replace(array("\r\n","\r","\n"), "<br>", $_POST["pre_message"]);
-        $preData = sprintf('<span class="date">%s</span><br>%s',$preDate, $preMessage);
-
-
-        $deleteKey = null;
+        $deleteKey = null; // 削除対象行のKey
         // ファイル内容配列を更新
         foreach ($fileArray as $key => $value) {
         	// 更新する行と一致すれば
@@ -250,22 +229,239 @@ class TextFile{
         		$deleteKey = $key;
         	}
         }
-
-        var_dump($deleteKey);
-        var_dump($fileArray);
-        array_splice($fileArray, $deleteKey, $deleteKey);
-		var_dump($fileArray);
-        // 書き込み
-        foreach($fileArray as $key => $value){
-            fwrite($fp,$value);
+        if($deleteKey === null){
+            $this->result = "ファイルの削除対象行 が見つかりませんでした";
+            return false;
         }
 
-        flock($fp, LOCK_UN); // ファイルロックを解放
-        fclose($fp);         // ファイルをクローズする
+        // 対象行を削除する
+        // array_splice($fileArray, $deleteKey, $deleteKey);
+        $fileArray[$deleteKey] = "";
 
+        // ファイル削除   ファイルアップロード時のみ
+        if($_POST["pre_image_name"]){
+            unlink( TextFile::FILE_SAVE_PATH . $_POST["pre_image_name"] );
+        }
+
+        // ファイルオープン と　ロック
+        $fp = $this->fileOpenAndLock(TextFile::FILE_WRITE_MODE);
+        if(!$fp) return false;
+
+        // 書き込み
+        foreach($fileArray as $key => $value){
+            // 空文字であれば書き込まない
+            if( $value == "") continue;
+            // 改行コードを行の最後に追加
+            $value = $value . "\n";
+            // 書き込み
+            fwrite($fp,$value);
+        }
+        // ファイルクローズ と アンロック
+        if( $this->fileCloseAndUnlock($fp) ) return false;
         //----------------------------------
 
+        $this->result = "ファイルの対象行 削除に成功しました";
         return true;
 	}
+
+//
+//
+// ↓↓↓↓↓　からはこのクラス内のみで使う共通関数を定義する。
+//
+// 基本的にエラーが発生した場合は、falseを返すようにしています。
+
+
+    //-------------------------
+    //  ファイルオープン、ロック
+    //-------------------------
+    private function fileOpenAndLock($mode){
+
+        $modeValue = null;
+        switch ($mode) {
+            case TextFile::FILE_READ_MODE:
+                $modeValue = 'r';
+                break;
+            
+            case TextFile::FILE_WRITE_MODE:
+                $modeValue = 'w';
+                break;
+
+            case TextFile::FILE_ADD_MODE:
+                $modeValue = 'ab';
+                break;
+        }
+
+        // ファイルオープン
+        $fp = fopen( TextFile::FILE_PATH, $modeValue);
+        if( !$fp ){
+            $this->result = "ファイルオープンに失敗しました";
+            return false;
+        }
+
+        // ファイルロック
+        $lockResult = flock($fp, LOCK_SH);
+        if( !$lockResult ){
+            $this->result = "ファイルロックに失敗しました";
+            return false;
+        }
+
+        return $fp;
+    }
+
+    //-----------------------------
+    //  ファイルクローズ、アンロック
+    //-----------------------------
+    private function fileCloseAndUnlock($fp){
+        // ファイルロックを解放
+        $unlockResult = flock($fp, LOCK_UN); 
+        if( !$unlockResult ){
+            $this->result = "ファイルロック解除に失敗しました";
+            return false;
+        }
+
+        // ファイルをクローズする
+        $closeResult = fclose($fp);
+        if( !$closeResult ){
+            $this->result = "ファイルクローズに失敗しました";
+            return false; 
+        }
+    }
+
+    //-----------------------------
+    //  ファイル内容取得メソッド
+    //  返り値 : 改行コードごとの配列
+    //-----------------------------
+    private function getFileValueArray() {
+
+        // テキストファイルの全内容を文字列で取得
+        $fileValue = file_get_contents(TextFile::FILE_PATH);
+        // 改行コードを \n にすべて置き換える
+        str_replace(array("\r\n","\r"), "\n", $fileValue);
+        // 改行コードごとに、文字列を分割
+        $fileArray = split("\n",$fileValue);
+
+        return $fileArray;
+    }
+
+    //---------------------------------
+    //  更新、削除で使う 変更前データ取得
+    //---------------------------------
+    private function getPreData() {
+
+        $preDate      = $_POST["pre_date"];
+        $preMessage   = str_replace(array("\r\n","\r","\n"), "<br>", $_POST["pre_message"]);
+        $preImageName = $_POST["pre_image_name"];
+        $preImagePos  = $_POST["pre_image_pos"];
+        if($preImagePos == TextFile::IMAGE_POS_TOP){
+            $preData = sprintf(TextFile::WRITE_TAG_STR1, $preDate, $preImageName, $preMessage);
+        }else{
+            $preData = sprintf(TextFile::WRITE_TAG_STR2, $preDate, $preMessage, $preImageName);
+        }
+
+        return $preData;
+    }
+
+    //------------------------------
+    //  更新で使う 変更後データ取得
+    //------------------------------
+    private function getUpdateData(){
+
+        $this->date      = $_POST["date"];
+        $this->message   = str_replace(array("\r\n","\r","\n"), "<br>", $_POST["message"]);
+
+        // ファイルアップロード
+        if( !$this->fileUpload() ) return false;
+
+        $this->imagePos = $_POST["image_pos"];
+        if($this->imagePos == TextFile::IMAGE_POS_TOP){
+            $updateData = sprintf(TextFile::WRITE_TAG_STR1, $this->date, $this->imageName, $this->message);
+        }else{
+            $updateData = sprintf(TextFile::WRITE_TAG_STR2, $this->date, $this->message, $this->imageName);
+        }
+
+        return $updateData;      
+
+    }
+
+    //-----------------------------
+    // ファイルアップロードメソッド
+    //-----------------------------
+    private function fileUpload(){
+
+        // 必須ではない場合、未選択であればファイルアップロードしない。
+        if($_FILES["image_file"]["error"] == UPLOAD_ERR_NO_FILE){
+            return true;
+        }
+
+        // エラーチェック
+        if( $this->errorCheck() ) return false;
+
+        // ファイル名設定
+        date_default_timezone_set('Asia/Tokyo');
+        $uploadDate = date('YmdHis', time() );
+        $extension = pathinfo($_FILES["image_file"]["name"], PATHINFO_EXTENSION);  // 拡張子を取得。 exctension = 拡張子
+        $fileName = $uploadDate . "." . $extension;
+        $this->imageName = $fileName;
+
+        // ファイルアップロード
+        $moveResult = move_uploaded_file($_FILES["image_file"]["tmp_name"], TextFile::FILE_SAVE_PATH.$fileName);
+        if(!$moveResult){
+            $this->result = "ファイルアップロードに失敗しました";
+            return false;
+        }
+        return true;
+    }
+
+    //-------------------------------------
+    // ファイルアップロード時のエラーチェック
+    //-------------------------------------
+    private function errorCheck(){
+
+        // $_FILES['file']['error'] アップロード後のエラーコードが設定されている。
+        switch( $_FILES["image_file"]["error"] ){
+            case UPLOAD_ERR_OK: // ファイルアップロード成功
+                break;
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                $this->result .= "ファイルの最大サイズを超えています。";
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $this->result .= "アップロードされたファイルは一部のみしかアップロードされていません。";
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $this->result .= "ファイルが選択されていない可能性があります。";
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+                $this->result .= "テンポラリフォルダがありません。";
+                break;
+            case UPLOAD_ERR_CANT_WRITE:
+                $this->result .= "ディスクへの書き込みに失敗しました。";
+                break;
+            default: //上記以外のエラーコードが返された場合
+                $this->result .= "ファイルアップロードに失敗しました。";
+                break;
+        }
+
+        // 拡張子チェック
+        if($this->result == ""){ //エラーがなければチェックするという条件
+            // アップロードファイル名を取得
+            $uploadFileName = $_FILES["image_file"]["name"];
+            // 拡張子を取得。 exctension = 拡張子
+            $extension = pathinfo($uploadFileName, PATHINFO_EXTENSION);
+            // 拡張子チェック
+            $errorChkFlg = false;
+            foreach ($this->errorChkExtension as $value) {
+                if( $extension == $value ){
+                    $errorChkFlg = true;
+                    break;
+                }
+            }
+            if(!$errorChkFlg) $this->result .= "画像以外のファイルをアップロードしています。";
+        }
+
+        // エラーがあればtrue, なければ false
+        $errorFlg = ($this->result != "") ? true : false;
+        return $errorFlg;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -18,11 +18,9 @@
     <script>
         $(function() {
             $.datepicker.setDefaults( $.datepicker.regional[ "ja" ] );
-            $( "#datepicker" ).datepicker();
+            $( "#datepicker" ).datepicker(); 
         });
     </script>
-
-
 </head>
 
 
@@ -33,29 +31,33 @@
 
     // テキストファイルクラスの作成
     $textFile = new TextFile();
+
+    // POST送信された場合
+    if ( $_SERVER["REQUEST_METHOD"] == "POST" ){
+        switch($_POST["process_type"]){
+            case 'regist':
+                $textFile->regist();
+                break;
+            case 'update':
+                $textFile->update();
+                break;
+            case 'delete':
+                $textFile->delete();
+                break;            
+        }
+    }
 ?>
     <div class="container">
-        <?php
-            
-            if ( $_SERVER["REQUEST_METHOD"] == "POST" ){
-                switch($_POST["PROCESS_TYPE"]){
-                    case 'regist':
-                        $textFile->regist();
-                        break;
-                    case 'update':
-                        $textFile->update();
-                        break;
-                    case 'delete':
-                        $textFile->delete();
-                        break;            
-                }
-            }
-        ?>
+        <h4>
+            <?php echo $textFile->result; ?>
+        </h4>
         <h2>sample.txtのファイル内容</h2>
 
+        <!-- ファイル内容のテーブルを表示 -->
         <?php
             $textFile->viewFileValue(); 
         ?>
+        <!-- End ファイル内容のテーブルを表示 -->
 
         <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#formModal" onClick="doChangeRegistForm();">
           登録
@@ -64,9 +66,9 @@
         <div class="modal fade" id="formModal">
           <div class="modal-dialog">
             <div class="modal-content">
-                <form action="test.php" method="POST" role="form">
-                <div class="modal-header">
-                    <h4 class="modal-title">Modal title</h4>
+                <form action="index.php" method="POST" role="form" enctype="multipart/form-data">
+                <div class="modal-header" style="text-align: center;">
+                    <h4 class="modal-title"><span id="formModalTitle">Modal title</span></h4>
                 </div>
                 <div class="modal-body">
                     <div class="form-group">
@@ -77,14 +79,28 @@
                         <label for="メッセージ欄">メッセージ欄</label>
                         <textarea name="message" rows="4" cols="40" class="form-control">メッセージ欄</textarea>
                     </div>
+                    <div class="form-group">
+                        <label for="画像">画像</label>
+                         <input type="hidden" name="MAX_FILE_SIZE" value="200000" />
+                        <input name="image_file" type="file"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="画像の表示位置">画像の表示位置</label><br>
+                        <select name="image_pos">
+                            <option value="<?php echo TextFile::IMAGE_POS_TOP ?>">文章の上に画像を表示</option>
+                            <option value="<?php echo TextFile::IMAGE_POS_BOTTOM ?>">文章の下に画像を表示</option>
+                        </select>
+                    </div>
                     <input type="hidden" name="pre_date" value="">
                     <input type="hidden" name="pre_message" value="">
-                    <input type="hidden" name="PROCESS_TYPE" value="">
+                    <input type="hidden" name="pre_image_pos" value="">
+                    <input type="hidden" name="pre_image_name" value="">
+                    <input type="hidden" name="process_type" value="">
                 </div>
                 <div class="modal-footer">
-                    <button type="submit" class="btn btn-default" id="regist" onClick="doRegist();">登録</button>
-                    <button type="submit" class="btn btn-default" id="update" onClick="doUpdate();">更新</button>
-                    <button type="submit" class="btn btn-default" id="delete" onClick="doDelete();">削除</button>
+                    <button type="submit" class="btn btn-default" id="regist" onClick="return doRegist();">登録</button>
+                    <button type="submit" class="btn btn-default" id="update" onClick="return doUpdate();">更新</button>
+                    <button type="submit" class="btn btn-default" id="delete" onClick="return doDelete();">削除</button>
                     <button type="button" class="btn btn-default" data-dismiss="modal">閉じる</button>
                 </div>
                 </form>
@@ -95,36 +111,69 @@
     </div>
     <script type='text/javascript'>
         function doRegist(){
-            document.forms[0].elements['PROCESS_TYPE'].value = 'regist';
+            if( !errorCheck() ) return false; 
+            document.forms[0].elements['process_type'].value = 'regist';
             doSubmit();
+            return true;
         }
         function doUpdate(){
-            document.forms[0].elements['PROCESS_TYPE'].value = 'update';
-            doSubmit();    
+            if( !errorCheck() ) return false; 
+            document.forms[0].elements['process_type'].value = 'update';
+            doSubmit();
+            return true;   
         }
         function doDelete(){
-            document.forms[0].elements['PROCESS_TYPE'].value = 'delete';
+            if( !errorCheck() ) return false; 
+            document.forms[0].elements['process_type'].value = 'delete';
             doSubmit(); 
-        }            
+            return true;
+        }
         function doSubmit(){
             document.forms[0].submit();
         }
 
+        function errorCheck(){
+            var date = document.forms[0].elements['date'].value;
+            if(date == ''){
+                alert("日付が未入力です。");
+                return false;
+            }
+            if( !date.match(/[0-9]{4}\/[0-9]{2}\/[0-9]{2}/) ){
+                alert("日付の形式が間違っています。 (ex 2000/01/01)");
+                return false;
+            }
+            return true;
+        }
         function doChangeRegistForm(){
+            document.getElementById( "formModalTitle" ).innerHTML = "登録画面";
             document.getElementById( "regist" ).style.display = 'inline-block';
             document.getElementById( "update" ).style.display = 'none';
             document.getElementById( "delete" ).style.display = 'none';
+
+            // フォームの内容を初期化。
+            document.forms[0].reset();
         }
-        function doChangeUpdateForm(date, message){
+        function doChangeUpdateForm(date, message, image_name, image_pos){
+            document.getElementById( "formModalTitle" ).innerHTML = "更新・削除画面";
             document.getElementById( "regist" ).style.display = 'none';
             document.getElementById( "update" ).style.display = 'inline-block';
             document.getElementById( "delete" ).style.display = 'inline-block';
 
             document.forms[0].elements['date'].value    = date;
             document.forms[0].elements['message'].value = message;
+            switch(image_pos){
+                case 0:
+                    document.forms[0].elements['image_pos'].selectedIndex = 0;
+                    break;
+                case 1:
+                    document.forms[0].elements['image_pos'].selectedIndex = 1;
+                    break;
+            }
 
             document.forms[0].elements['pre_date'].value    = date;
             document.forms[0].elements['pre_message'].value = message;
+            document.forms[0].elements['pre_image_name'].value = image_name;
+            document.forms[0].elements['pre_image_pos'].value = image_pos;
         }
     </script>
 
